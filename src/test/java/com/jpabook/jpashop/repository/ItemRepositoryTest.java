6222a48a92b123f3f3999677789680a2c5bbbdf7
taisukim <cookehwk@naver.com>
2023-03-08 00:00:23 +0900

    day 4 real end

--- src/test/java/com/jpabook/jpashop/repository/ItemRepositoryTest.java
+++ src/test/java/com/jpabook/jpashop/repository/ItemRepositoryTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.NonUniqueResultException;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional(readOnly = true)
@@ -38,6 +42,11 @@ class ItemRepositoryTest {
      * repository 에서 getSingleResult 로 찾았을경우
      * 만약 결과가 2개면 어떤식으로 에러가 발생되는지 또는 에러가 발생되지 않는다면 어떤식으로 작동되는지
      * 확인하려고 테스트코드를 작성해보았다.
+     *
+     * 추가로 중복예외를 처리하는 테스트메서드를 30분정도 찾아봤지만 찾지못했다.
+     * 사실 이 테스트코드에서 발생하는 에러는 domain 에 unique 를 설정하면 발생하지 않을것이다
+     * 굳이 어거지로 예외를 발생시키는걸 테스트로 작성하는게 맞나 싶다..
+     * 아마 찾아보면 중복예외를 처리하는 메서드가 있긴할것같은데 졸령
      */
     @Test
     @Transactional
@@ -46,6 +55,11 @@ class ItemRepositoryTest {
         Item item2 = Album.createAlbum("album", 2000, 20, "taesoo2", "ect2");
         itemRepository.save(item);
         itemRepository.save(item2);
+
+//        Assertions.assertThrows(IncorrectResultSizeDataAccessException.class, () -> itemRepository.findByName(item.getName()));
+        assertThatThrownBy(() -> itemRepository.findByName(item.getName()))
+                .isInstanceOf(NonUniqueResultException.class)
+                .isInstanceOf(IncorrectResultSizeDataAccessException.class);
 
         Optional<Item> existItem = itemRepository.findByName(item.getName());
         Item album = null;
